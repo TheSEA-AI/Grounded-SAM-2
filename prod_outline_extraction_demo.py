75e83670345893ae9e615c4dae31f9e2839626fc
YangMI-YM <yang.mi@thesea.ai>
2025-05-21 06:32:49 +0000

    demo for auto-labeling added

---
+++ prod_outline_extraction_demo.py
@@ -0,0 +1,349 @@
+##### for extracting hed images where the inner lines of produts are removed
+
+import os, sys
+import gc
+
+import argparse
+import copy
+from pathlib import Path
+
+#from IPython.display import display
+from PIL import Image, ImageDraw, ImageFont
+from torchvision.ops import box_convert
+
+# Grounding DINO and SAM2
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+from grounding_dino.groundingdino.util.inference import load_model, load_image, predict
+#from transformers import pipeline
+
+import supervision as sv
+from scipy import ndimage
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from typing import Union
+
+## annotation
+sys.path.append('/home/yangmi/ControlNet') # pth to annotator
+from annotator.hed import HEDdetector, nms
+from annotator.util import HWC3, resize_image
+
+# diffusers
+import PIL
+import requests
+import torch
+from io import BytesIO
+from torchvision import transforms
+
+import locale
+locale.getpreferredencoding = lambda: "UTF-8"
+import traceback
+
+def parse_args(input_args=None):
+    parser = argparse.ArgumentParser(description="Simple example of product position extraction based on Grouned SAM.")
+
+    parser.add_argument("--input_dir", 
+                        default=None, 
+                        type=str, 
+                        required=True, 
+                        help="Path to data instance.")
+
+    parser.add_argument("--output_dir", 
+                        default=None, 
+                        type=str, 
+                        required=True, 
+                        help="Path to data hed background.")
+
+    parser.add_argument("--image_resolution", 
+                        default=1024, 
+                        type=int, 
+                        help="Size of output image.")
+    
+    parser.add_argument("--img_format", 
+                        default='png', 
+                        type=str, 
+                        help="Path to the image.")
+
+    parser.add_argument("--sam2_checkpoint_pth", 
+                        default=None, 
+                        type=str, 
+                        help="Path to the preloaded SAM2 model checkpoint.")
+
+    parser.add_argument("--sam2_config", 
+                        default=None, 
+                        type=str, 
+                        help="Name of SAM2 model configuration *yaml that matches the SAM2 model. Make sure your config is already under the Hydra search path which can be view via ```pip show SAM-2```")
+
+    parser.add_argument("--gdino_checkpoint_pth", 
+                        default=None, 
+                        type=str, 
+                        help="Path to the preloaded grounding-dino model checkpoint.")
+
+    parser.add_argument("--gdino_config_pth", 
+                        default=None, 
+                        type=str, 
+                        help="Path to the grounding-dimo model configuration *cfg.py.")
+    
+    parser.add_argument("--gpu_id", 
+                        default=0, 
+                        type=int, 
+                        required=False,
+                        help="gpu id")
+
+    parser.add_argument("--product_images",
+                    nargs='+', 
+                    default=None,
+                    required=False,
+                    help="The background image with the product")
+
+    parser.add_argument("--similarity_threshold", 
+                        default=3.0, #0.916, 
+                        type=float, 
+                        required=False,
+                        help="The threshold to remove hed images")
+
+    parser.add_argument("--hed_value", 
+                        default=190, 
+                        type=int, 
+                        required=False,
+                        help="The hed value for product")
+    
+    parser.add_argument("--candidate_num",
+                        default=2, 
+                        type=int, 
+                        required=False,
+                        help="The number of candidates for data hed background images")
+
+    if input_args is not None:
+        args = parser.parse_args(input_args)
+    else:
+        args = parser.parse_args()
+    
+    return args
+
+
+def product_outline_extraction_by_mask_multiple_product_types(args, grounding_model, sam2_predictor, device='cuda'):
+
+    Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+
+    image_filename_list = [i for i in os.listdir(args.input_dir)]
+    images_path = [os.path.join(args.input_dir, file_path)
+                        for file_path in image_filename_list if file_path.endswith('.png')]
+
+    hedDetector = HEDdetector()
+    kernel = np.ones((3, 3), np.uint8)
+    image_dim = args.image_resolution
+    for img_path, img_name in zip(images_path, image_filename_list):       
+        #####################################
+        # image load
+        image_raw = Image.open(img_path)
+        if image_raw.mode in ('RGBA', 'LA') or (image_raw.mode == 'P' and 'transparency' in image_raw.info):
+            # Create a white background image of the same size
+            img = Image.new('RGBA', image_raw.size, (255, 255, 255, 255))  # White background
+            # Paste the image on the white background using the alpha channel as a mask
+            image_raw = image_raw.convert('RGBA')
+            img.paste(image_raw, mask=image_raw.split()[3])
+            # Convert the image to RGB mode (to remove the alpha channel)
+            img = img.convert('RGB')
+        else:
+            # If the image doesn't have transparency, no change is needed
+            img = image_raw.convert('RGB')
+
+        image_array = np.asarray(img)
+
+        #####################################
+        #extract mask
+        image_source, image = load_image(img_path, image_dim)
+        sam2_predictor.set_image(image_source)
+        product_types = ["beauty product", "cosmetic product", "skincare product", "makeup product", "personal care product"]
+        mask_prods = np.full((image_source.shape[1], image_source.shape[1]), True, dtype=bool)
+        prod_edges = []
+        for product_type in product_types:
+            boxes, _, _ = predict(
+                model=grounding_model,
+                image=image,
+                caption=product_type,
+                box_threshold=0.35,
+                text_threshold=0.25,
+                #device=device,
+            )
+
+            # process the box prompt for SAM 2
+            h, w, _ = image_source.shape
+            boxes = boxes * torch.Tensor([w, h, w, h])
+            input_boxes = box_convert(boxes=boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
+            #mask_all = np.full((image_source.shape[1],image_source.shape[1]), True, dtype=bool)
+            if boxes.size(0) != 0:
+                masks, _, _ = sam2_predictor.predict(
+                    point_coords=None,
+                    point_labels=None,
+                    box=input_boxes,
+                    multimask_output=False,
+                )
+
+                """
+                Post-process the output of the model to get the masks, scores, and logits for visualization
+                """
+                # convert the shape to (n, H, W)
+                if masks.ndim == 4:
+                    masks = masks.squeeze(1)
+                prod_id = 0
+                for mask in masks:
+                    im = np.stack((mask,)*3, axis=-1)
+                    im = im.astype(np.uint8)*255
+                    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+                    _, thresh = cv2.threshold(imgray, 127, 255, 0)
+                    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    if len(contours) >= 50:
+                        continue
+                    mask_prods = mask_prods & ~mask.astype(bool)
+
+                    mask_prod = np.full((image_source.shape[1],image_source.shape[1]), True, dtype=bool) & ~mask.astype(bool)
+
+                    ##### fill holes inside product #######
+                    mask_prod = ~mask_prod
+                    mask_prod = mask_prod.astype(int)
+                    mask_prod = ndimage.binary_fill_holes(mask_prod).astype(int)
+                    mask_prod = mask_prod.astype(bool)
+                    mask_prod = ~mask_prod
+                    ##### fill holes inside product #######
+
+                    ##### fill small holes outside product #######
+                    ite = 8
+                    mask_prod = mask_prod.astype(int)
+                    mask_prod = ndimage.binary_closing(mask_prod, iterations=ite).astype(int)
+                    mask_prod = mask_prod.astype(bool)
+                    ##### fill small holes outside product #######
+
+                    ##### flip surrounding pixels due to previous fill small holes outside product #######
+                    mask_prod[0:ite+2, :] = True
+                    mask_prod[:, 0:ite+2] = True
+                    mask_prod[image_dim-ite-1:, :] = True
+                    mask_prod[:, image_dim-ite-1:] = True
+                    ##### flip surrounding pixels due to previous fill small holes outside product #######
+
+                    mask_prod = np.stack((mask_prod,)*3, axis=-1)
+                    ################
+
+                    mask_expand = ~mask_prod
+                    mask_expand = mask_expand.astype(np.uint8)
+                    mask_expand = cv2.dilate(mask_expand, kernel, iterations=3)
+                    mask_expand = np.array(mask_expand, dtype=bool)
+        
+                    #white_array = np.ones_like(image_array) * args.hed_value
+                    white_array = np.ones((image_dim, image_dim, 3), dtype=np.uint8) * args.hed_value
+                    white_array = white_array * mask_prod
+                    white_array = white_array * mask_expand
+
+                    # save white_array
+                    prod_edges.append(white_array)
+            else:
+                print(f"the product outline of {product_type} in {img_name} cannot be extracted.")
+                #raise ValueError(f"the product outline in {img_name} cannot be extracted.")
+
+        ##### fill holes inside product #######
+        mask_prods = ~mask_prods
+        mask_prods = mask_prods.astype(int)
+        mask_prods = ndimage.binary_fill_holes(mask_prods).astype(int)
+        mask_prods = mask_prods.astype(bool)
+        mask_prods = ~mask_prods
+        ##### fill holes inside product #######
+
+        ##### fill small holes outside product #######
+        ite = 8
+        mask_prods = mask_prods.astype(int)
+        mask_prods = ndimage.binary_closing(mask_prods, iterations=ite).astype(int)
+        mask_prods = mask_prods.astype(bool)
+        ##### fill small holes outside product #######
+
+        ##### flip surrounding pixels due to previous fill small holes outside product #######
+        mask_prods[0:ite+2, :] = True
+        mask_prods[:, 0:ite+2] = True
+        mask_prods[image_dim-ite-1:, :] = True
+        mask_prods[:, image_dim-ite-1:] = True
+        ##### flip surrounding pixels due to previous fill small holes outside product #######
+
+        mask_prods = np.stack((mask_prods,)*3, axis=-1)
+        ################
+
+        mask_inv = ~mask_prods
+        mask_inv = mask_inv.astype(np.uint8)
+        mask_inv = cv2.dilate(mask_inv, kernel, iterations=3)
+        mask_inv = np.array(mask_inv, dtype=bool)
+
+        hed = HWC3(image_array)
+        hed = hedDetector(hed) 
+        hed = cv2.resize(hed, (args.image_resolution, args.image_resolution), interpolation=cv2.INTER_LINEAR)
+        # DEBUGGING propose: save hed in case seg fails
+        cv2.imwrite(os.path.join(args.output_dir, '.'.join(img_name.split('.')[:-1]) + f"_hed.png"), hed)
+        hed = hed * mask_prods[:,:,0]
+        hed = hed * mask_inv[:,:,0]
+        hed = HWC3(hed)
+        for white_array in prod_edges:
+            hed = np.where(white_array>0, white_array, hed)
+        hed[hed > 60] = args.hed_value
+        hed[hed <= 60] = 0
+
+        #hed = cv2.resize(hed, (args.image_resolution, args.image_resolution), interpolation=cv2.INTER_LINEAR)
+        img_masked = Image.fromarray(hed)
+        img_save_path = args.output_dir + '/' + '.'.join(img_name.split('.')[:-1]) + f"_edges.png"
+        img_masked.save(img_save_path, args.img_format)
+
+
+##### for extracting hed images where the inner lines of produts are removed
+# usage: 
+# python prod_hed_outline_extraction_demo.py --gpu_id 4 --input_dir /home/ec2-user/server/Production/Projects/666666/controlnola/ --output_dir /home/ec2-user/s3data/hed_extract_test
+if __name__ == "__main__":
+
+    dummy_paths = {
+        "prod_img_hard": "/home/yangmi/thesea-webserver/Production/Projects/666666/controlnola/HELENA RUBINSTEIN/Powercell Skinmunity Serum Pump/Generated Image/PRADA-28-perfume-floating",
+        "prod_img": "/home/yangmi/thesea-webserver/Production/Projects/666666/controlnola/PRADA Beauty/Luna Rossa Carbon Cologne Perfume/Generated Image/1725087664.7395656",
+        "ref_prod_hard": "/home/yangmi/thesea-webserver/Production/Projects/666666/controlnola/HELENA RUBINSTEIN/Powercell Skinmunity Serum Pump/data_product_transparent",
+        "ref_prod": "/home/yangmi/thesea-webserver/Production/Projects/666666/controlnola/PRADA Beauty/Luna Rossa Carbon Cologne Perfume/data_product_transparent",
+    }
+    args = parse_args()
+    # TODO # FIXME REMOVE setattr when using this script
+    setattr(args, "input_dir", dummy_paths["ref_prod_hard"]) # choosing from dummy_paths["prod_img"] and dummy_paths["ref_prod"]
+    setattr(args, "output_dir", "/home/yangmi/s3data/hed_extract_test/demo_output")
+    
+    device = torch.device(args.gpu_id) if args.gpu_id>=0 else 'cpu'
+
+    if device != 'cpu':
+        # use float16 for the entire notebook
+        torch.autocast(device_type="cuda:"+str(args.gpu_id), dtype=torch.float16).__enter__()
+        torch.autocast(device_type="cuda:0", dtype=torch.float16).__enter__()
+        #torch.autocast(device_type="cuda", dtype=torch.float16).__enter__()
+    
+        if torch.cuda.get_device_properties(0).major >= 8:
+            # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+            torch.backends.cuda.matmul.allow_tf32 = True
+            torch.backends.cudnn.allow_tf32 = True
+        
+    try:
+        # build SAM2 image predictor
+        sam2_checkpoint = "./checkpoints/sam2.1_hiera_base_plus.pt" if args.sam2_checkpoint_pth is None else args.sam2_checkpoint_pth
+        model_cfg = "sam2.1_hiera_b+.yaml" if args.sam2_config is None else args.sam2_config
+        sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+        sam2_predictor = SAM2ImagePredictor(sam2_model)
+
+        # build grounding dino model
+        model_id = "IDEA-Research/grounding-dino-base"
+        grounding_model = load_model(
+            model_config_path="./grounding_dino/groundingdino/config/GroundingDINO_SwinB_cfg.py" if args.gdino_config_pth is None else args.gdino_config_pth, 
+            model_checkpoint_path="./gdino_checkpoints/groundingdino_swinb_cogcoor.pth" if args.gdino_checkpoint_pth is None else args.gdino_checkpoint_pth,
+            device=device
+        )
+        
+        product_outline_extraction_by_mask_multiple_product_types(args, grounding_model, sam2_predictor, device=device)
+        
+        print(f'product outline extraction process finished.')
+    except:
+        traceback.print_exc()
+    finally:
+        del sam2_model
+        del sam2_predictor
+        del grounding_model
+        gc.collect()
+        torch.cuda.empty_cache()
